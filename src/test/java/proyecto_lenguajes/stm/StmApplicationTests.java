97fd701e780dadff945061cd70067df3a8df756d
Totality123 <steven2004medina@gmail.com>
2025-04-11 11:44:19 -0600

    Proyecto inicial

---
+++ src/test/java/proyecto_lenguajes/stm/StmApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_lenguajes.stm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
